4f9eecba348f332149b2dfd597b2cb9b03849f6d
KalaQuest <malakooku@gmail.com>
2018-08-01 11:23:24 -0600

    added a few shapes and set up the grid columns&rows.

--- src/components/app.js
+++ src/components/app.js
@@ -3,8 +3,14 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <h1>DevCamp React Starter</h1>
+      <div className="grid">
+        <div>h1</div>
+        <div>h1</div>
+        <div>h1</div>
+        
+        <div className="grid__skewlighton">h1</div>
+        <div className="grid__skewlightonn">h1</div>
+        <div className="grid__skewlightbox">h1</div>
       </div>
     );
   }
