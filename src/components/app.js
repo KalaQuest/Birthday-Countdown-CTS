4f907d605d075ffffed8ab7bd979d2fe79bd31f7
KalaQuest <malakooku@gmail.com>
2018-08-01 12:51:54 -0600

    added the title thingy

--- src/components/app.js
+++ src/components/app.js
@@ -4,13 +4,14 @@ export default class App extends Component {
   render() {
     return (
       <div className="grid">
+        <h1 className="grid__title">"Birthday Countdown"?</h1>
         <div className="grid__skewdarkbox"></div>
         <div className="grid__skewdarkonn"></div>
         <div className="grid__skewdarkonnn"></div>
         
-        <div className="grid__skewlighton">h1</div>
-        <div className="grid__skewlightonn">h1</div>
-        <div className="grid__skewlightbox">h1</div>
+        <div className="grid__skewlighton"></div>
+        <div className="grid__skewlightonn"></div>
+        <div className="grid__skewlightbox"></div>
       </div>
     );
   }
