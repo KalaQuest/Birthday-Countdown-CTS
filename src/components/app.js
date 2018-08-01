9d59b0b6b8936754029cf317b9d6551e3cb2b7c8
KalaQuest <malakooku@gmail.com>
2018-08-01 12:32:06 -0600

    placed some skews and messed with Css.

--- src/components/app.js
+++ src/components/app.js
@@ -4,9 +4,9 @@ export default class App extends Component {
   render() {
     return (
       <div className="grid">
-        <div>h1</div>
-        <div>h1</div>
-        <div>h1</div>
+        <div className="grid__skewdarkbox"></div>
+        <div className="grid__skewdarkonn"></div>
+        <div className="grid__skewdarkonnn"></div>
         
         <div className="grid__skewlighton">h1</div>
         <div className="grid__skewlightonn">h1</div>
