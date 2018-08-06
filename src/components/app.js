62af1b22071b0ea3efd2474025403bbc95505718
KalaQuest <malakooku@gmail.com>
2018-08-06 11:19:12 -0600

    recentered stuff. not much else.

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Picker from './picker';
+import Button from './button';
 
 export default class App extends Component {
   render() {
@@ -14,6 +15,7 @@ export default class App extends Component {
         <div className="grid__skewlightonn"></div>
         <div className="grid__skewlightbox"></div>
         <Picker/>
+        { Button('Generate Countdown?')}
       </div>
     );
   }
